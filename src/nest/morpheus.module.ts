cceba983df7760bc9fefe728891356ed284f3e79
Mariano Z <marianoz@zoho.com>
2022-03-03 10:21:44 -0300

    chore(morpheus.module): add return types

--- src/nest/morpheus.module.ts
+++ src/nest/morpheus.module.ts
@@ -64,7 +64,7 @@ export class MorpheusModule {
 
   private static createFactoryWrapper(
     useFactory: MorpheusModuleAsyncOptions['useFactory'],
-  ) {
+  ): (...args: any[]) => Promise<MorpheusService> {
     return async (...args: any[]) => {
       const clientOptions = await useFactory(...args);
       return new MorpheusService(clientOptions);
